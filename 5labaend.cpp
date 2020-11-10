7ffbc79bd37ca3e46aa668cccd8fa8941e678240
Eugenito <72038188+Eugenito@users.noreply.github.com>
2020-11-10 23:43:39 +0300

    Add files via upload

---
+++ 5labaend.cpp
@@ -0,0 +1,46 @@
+﻿#include <stdio.h>
+#include <iostream>
+int main()
+{
+	int m;
+	int array[3][3];
+	setlocale(LC_ALL, "Rus");
+	printf("Введите элементы массива\n");
+	for (int i = 0;i < 3;i++)
+	{
+		for (int j = 0;j < 3;j++)
+		{
+			scanf_s("%d ", &array[i][j]);
+		}
+	}
+	for (int i = 0;i < 3;i++)
+	{
+
+		for (int z = 0;z < 2;z++)
+		{
+			for (int j = 1;j < 3;j++)
+			{
+
+
+				if (array[i][z] < array[i][j])
+				{
+					m = array[i][z];
+					array[i][z] = array[i][j];
+					array[i][j] = m;
+				}
+
+			}
+		}
+
+
+	}
+	for (int i = 0;i < 3;i++)
+	{
+		printf("\n");
+		for (int j = 0;j < 3;j++)
+		{
+			printf("%d ", array[i][j]);
+		}
+	}
+	return 0;
+}
